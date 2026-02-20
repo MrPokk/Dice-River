b1927bb7d2212ded68a4ddfd91ded05439282454
Kirill <dr.mokrousoff2017@yandex.ru>
2026-02-20 20:47:42 +0300

    Кнопки звука

--- Assets/!Dice River/!Scripts/UI/Screen/UIToStartFloating.cs
+++ Assets/!Dice River/!Scripts/UI/Screen/UIToStartFloating.cs
@@ -1,9 +1,62 @@
 ﻿using UINotDependence.Core;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class UIToStartFloating : UIScreen
 {
+    [Header("UI Elements")]
+    public Toggle toggleMusic;
+    public Toggle toggleSfx;
+
+    [Header("Audio")]
+    public AudioMixer mixer;
+
+    private const string MIXER_MUSIC = "MusicVolume";
+    private const string MIXER_SFX = "SfxVolume";
+
+    private const string PREF_MUSIC_ON = "MusicEnabled";
+    private const string PREF_SFX_ON = "SfxEnabled";
+
+    public override void Open()
+    {
+        base.Open();
+
+        var isMusicOn = PlayerPrefs.GetInt(PREF_MUSIC_ON, 1) == 1;
+        var isSfxOn = PlayerPrefs.GetInt(PREF_SFX_ON, 1) == 1;
+
+        toggleMusic.isOn = isMusicOn;
+        toggleSfx.isOn = isSfxOn;
+
+        SetVolume(MIXER_MUSIC, isMusicOn);
+        SetVolume(MIXER_SFX, isSfxOn);
+
+        toggleMusic.onValueChanged.AddListener(OnMusicToggle);
+        toggleSfx.onValueChanged.AddListener(OnSfxToggle);
+    }
+
     public override void Close()
     {
+        toggleMusic.onValueChanged.RemoveAllListeners();
+        toggleSfx.onValueChanged.RemoveAllListeners();
         base.Close();
+    }
+
+    private void OnMusicToggle(bool isOn)
+    {
+        SetVolume(MIXER_MUSIC, isOn);
+        PlayerPrefs.SetInt(PREF_MUSIC_ON, isOn ? 1 : 0);
+    }
+
+    private void OnSfxToggle(bool isOn)
+    {
+        SetVolume(MIXER_SFX, isOn);
+        PlayerPrefs.SetInt(PREF_SFX_ON, isOn ? 1 : 0);
+    }
+
+    private void SetVolume(string parameter, bool isOn)
+    {
+        var targetVolume = isOn ? 0f : -80f;
+        mixer.SetFloat(parameter, targetVolume);
     }
 }
